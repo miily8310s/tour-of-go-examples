9c25e37c14f3f2df33ba326e9c12e055ba7f7990
miily8310s <rillakerika@gmail.com>
2021-06-28 20:56:21 +0900

    add multiple results

--- basics.go
+++ basics.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func add(x, y int) int {
+	return x * y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println(math.Pi)
+	fmt.Println(add(33, 11))
+	fmt.Println(swap("hello", "house"))
 }
