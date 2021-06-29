f73a63874b045d7782c94f2116bb521070ec72da
miily8310s <rillakerika@gmail.com>
2021-06-29 21:52:28 +0900

    Add flow

--- basics.go
+++ basics.go
@@ -14,6 +14,7 @@ func swap(x, y string) (string, string) {
 }
 
 func main() {
+	// Basics
 	fmt.Println("Hello, World!")
 	fmt.Println(math.Pi)
 	fmt.Println(add(33, 11))
@@ -21,4 +22,15 @@ func main() {
 	var a, b = swap("hello", "world")
 	c, d := swap("hello", "world")
 	fmt.Println(a, b, c, d)
+
+	// Flow
+	sum := 0
+	for i:=0; i < 10; i++ {
+		sum +=1
+	}
+	fmt.Println(sum)
+
+	if v := 2; v < 6 {
+		fmt.Println("Yoshi")
+	}
 }
